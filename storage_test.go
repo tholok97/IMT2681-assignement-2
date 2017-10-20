e0b89018fbb50b2a446292f27a654e5e773072c8
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-10-21 00:50:25 +0200

    tests for volatile db

--- storage_test.go
+++ storage_test.go
@@ -1,77 +1,148 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func generateTestSubscriber(url string) Subscriber {
 	return Subscriber{
-		webhookURL:      url,
-		baseCurrency:    "TEST",
-		targetCurrency:  "TEST",
-		minTriggerValue: 923,
-		maxTriggerValue: 923,
+		WebhookURL:      url,
+		BaseCurrency:    "TEST",
+		TargetCurrency:  "TEST",
+		MinTriggerValue: 923,
+		MaxTriggerValue: 923,
 	}
 }
 
-// tests the add functionality of a factory-new implementor of subscriberDB
-func DBAddTest(t *testing.T, db SubscriberDB) {
+func TestVolatileSubscriberDBFactory(t *testing.T) {
 
-	var count int // holds count in db
-	var err error // holds potential error
+	// make a new db
+	db := VolatileSubscriberDBFactory()
 
-	// generate the test-subscribers
+	// assert that fresh db is empty
+	if len(db.subscribers) != 0 {
+		t.Error("factory-fresh db not empty")
+	}
+}
+
+func TestVolatileSubscriberDB_Add(t *testing.T) {
+
+	// make a new db
+	db := VolatileSubscriberDBFactory()
+
+	// test subscribers
 	s1 := generateTestSubscriber("1")
 	s2 := generateTestSubscriber("2")
 
-	// Assert that the db is empty
-	count, err = db.Count()
-	if err != nil {
-		t.Error(err)
-	}
-	if count != 0 {
-		t.Error("Factory-new db is not empty")
+	// add test subscribers
+	id1, err1 := db.Add(s1)
+	id2, err2 := db.Add(s2)
+
+	if err1 != nil {
+		t.Error(err1)
+	} else if err2 != nil {
+		t.Error(err2)
 	}
 
-	// insert into db
-	_, err = db.Add(s1)
-	_, err = db.Add(s2)
-
-	// assert nothing went wrong
-	if err != nil {
-		t.Error(err)
+	// assert db size
+	if len(db.subscribers) != 2 {
+		t.Error("db-size isn't 2 after adding two things")
 	}
 
-	// assert that count after insert is 2
-	count, err = db.Count()
-	if err != nil {
-		t.Error(err)
-	}
-	if count != 2 {
-		t.Error("db count is not 2 after adding 2 subscribers")
+	// assert that the correct elements were inserted
+	if db.subscribers[id1] != s1 || db.subscribers[id2] != s2 {
+		t.Error("id returned from add not correct")
+		t.Error("id returned from add not correct")
 	}
 }
 
-// test the count functionality of a factory-new implementor of SubscriberDB
-func DBCountTest(t *testing.T, db SubscriberDB) {
+func TestVolatileSubscriberDB_Count(t *testing.T) {
 
-}
-
-func DBGetTest(t *testing.T, db SubscriberDB) {
-
-}
-
-func DBGetAllTest(t *testing.T, db SubscriberDB) {
-
-}
-
-// test all the features of a TestVolatileSubscriberDB
-func TestVolatileSubscriberDB(t *testing.T) {
-
-	// make a new db (This function should always work -> not tested)
+	// make a new db
 	db := VolatileSubscriberDBFactory()
 
-	// Test each of the functionalities
-	DBAddTest(t, &db)
-	DBCountTest(t, &db)
-	DBGetTest(t, &db)
-	DBGetAllTest(t, &db)
+	var count int
+	var err error
+
+	// assert that empty db returns Count() = 0
+	count, err = db.Count()
+	if err != nil {
+		t.Error(err)
+	} else if count != 0 {
+		t.Error("factory-fresh db not returning correct count (0)")
+	}
+
+	// add a few subscribers
+	db.subscribers[1] = generateTestSubscriber("NOT RELEVANT")
+	db.subscribers[2] = generateTestSubscriber("NOT RELEVANT")
+
+	// assert that Count() is now 2
+	count, err = db.Count()
+	if err != nil {
+		t.Error(err)
+	} else if count != 2 {
+		t.Error("count not 2 after adding 2 subscribers")
+	}
+}
+
+func TestVolatileSubscriberDB_Get(t *testing.T) {
+
+	var s Subscriber
+	var err error
+
+	// make a new db
+	db := VolatileSubscriberDBFactory()
+
+	// test subscriber
+	s1 := generateTestSubscriber("1")
+
+	// test id's
+	id1 := 2 // VALID
+	id2 := 3 // INVALID
+
+	// add test subscriber to db
+	db.subscribers[id1] = s1
+
+	// assert that correct subscriber is returned from valid id
+	s, err = db.Get(id1)
+	if err != nil {
+		t.Error(err)
+	} else if s != s1 {
+		t.Error("wrong subscriber returned from get")
+	}
+
+	// assert that error is returned for invalid id
+	_, err = db.Get(id2)
+	if err == nil {
+		t.Error("error not returned for invalid id")
+	}
+}
+
+func TestVolatileSubscriberDB_GetAll(t *testing.T) {
+
+	// make a new db
+	db := VolatileSubscriberDBFactory()
+
+	// make test subscribers
+	testSubs := []Subscriber{
+		generateTestSubscriber("1"),
+		generateTestSubscriber("2"),
+	}
+
+	// add test subscribers to db
+	for i, s := range testSubs {
+		db.subscribers[i] = s
+	}
+
+	// getall, and check for errors
+	all, err := db.GetAll()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// assert that the returned slice is equal to the test slice
+	if !(testSubs[0] == all[0] && testSubs[1] == all[1]) &&
+		!(testSubs[0] == all[1] && testSubs[1] == all[0]) {
+		t.Error("subs returned from getAll() not equal to test subs")
+	}
 }
