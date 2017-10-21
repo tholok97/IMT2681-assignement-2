2992c24edde52b98ee24f728f7f1198b14fed330
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-10-21 15:53:38 +0200

    added basic handling of incoming subscribers (POST)

--- subscriberHandler.go
+++ subscriberHandler.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // SubscriberHandler handles requests from clients. It has info on subscribers
 // and currency
@@ -16,19 +20,49 @@ func SubscriberHandlerFactory(db SubscriberDB) SubscriberHandler {
 }
 
 // handle subscriber requests
-func (db *SubscriberHandler) handleSubscriberRequest(res http.ResponseWriter, req *http.Request) {
-	status := http.StatusNotImplemented
-	http.Error(res, http.StatusText(status), status)
+func (handler *SubscriberHandler) handleSubscriberRequest(res http.ResponseWriter, req *http.Request) {
+
+	// switch on the method of the request
+	switch req.Method {
+	case "POST":
+
+		// attempt to decode the POST json
+		var s Subscriber
+		err := json.NewDecoder(req.Body).Decode(&s)
+
+		// if couldn't decode -> bad req
+		// (SHOULD ALSO FAIL FOR NON-COMPLIANT JSON)
+		if err != nil {
+			http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		// (try to) add the student
+		id, addErr := handler.db.Add(s)
+
+		// if couldn't add -> internal server error
+		//  (client's responsability to retry)
+		if addErr != nil {
+			http.Error(res, addErr.Error(), http.StatusInternalServerError)
+		}
+
+		// respond with id given by db
+		fmt.Fprint(res, id)
+	case "GET":
+	default:
+		status := http.StatusNotImplemented
+		http.Error(res, http.StatusText(status), status)
+	}
 }
 
 // handle requests about latests data
-func (db *SubscriberHandler) handleLatest(res http.ResponseWriter, req *http.Request) {
+func (handler *SubscriberHandler) handleLatest(res http.ResponseWriter, req *http.Request) {
 	status := http.StatusNotImplemented
 	http.Error(res, http.StatusText(status), status)
 }
 
 // handle requests about average data
-func (db *SubscriberHandler) handleAverage(res http.ResponseWriter, req *http.Request) {
+func (handler *SubscriberHandler) handleAverage(res http.ResponseWriter, req *http.Request) {
 	status := http.StatusNotImplemented
 	http.Error(res, http.StatusText(status), status)
 }
