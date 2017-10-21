b561661acf7ebcc84cfbc5f64a0028745599a04a
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-10-21 15:53:19 +0200

    fixed int -> float32 in payload structs

--- payload.go
+++ payload.go
@@ -2,20 +2,20 @@ package main
 
 // Subscriber holds info about a subscriber to currency-info in the system
 type Subscriber struct {
-	WebhookURL      string `json:"webhookURL"`
-	BaseCurrency    string `json:"baseCurrency"`
-	TargetCurrency  string `json:"targetCurrency"`
-	MinTriggerValue int    `json:"minTriggerValue"`
-	MaxTriggerValue int    `json:"maxTriggerValue"`
+	WebhookURL      string  `json:"webhookURL"`
+	BaseCurrency    string  `json:"baseCurrency"`
+	TargetCurrency  string  `json:"targetCurrency"`
+	MinTriggerValue float32 `json:"minTriggerValue"`
+	MaxTriggerValue float32 `json:"maxTriggerValue"`
 }
 
 // CurrencyPayload holds info sent to subscriber
 type CurrencyPayload struct {
-	BaseCurrency    string `json:"baseCurrency"`
-	TargetCurrency  string `json:"targetCurrency"`
-	CurrentRate     int    `json:"currentRate"`
-	MinTriggerValue int    `json:"minTriggerValue"`
-	MaxTriggerValue int    `json:"maxTriggerValue"`
+	BaseCurrency    string  `json:"baseCurrency"`
+	TargetCurrency  string  `json:"targetCurrency"`
+	CurrentRate     float32 `json:"currentRate"`
+	MinTriggerValue float32 `json:"minTriggerValue"`
+	MaxTriggerValue float32 `json:"maxTriggerValue"`
 }
 
 // CurrencyRequest holds info received from the user during basic currency
